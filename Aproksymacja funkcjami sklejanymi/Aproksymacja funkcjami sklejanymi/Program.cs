90b714e4508a0d2620cb3244d161a20e10344e2f
Eryk Olszewski <erix2211@wp.pl>
2020-09-14 15:31:15 +0200

    Dodanie projektu

---
+++ Aproksymacja funkcjami sklejanymi/Aproksymacja funkcjami sklejanymi/Program.cs
@@ -0,0 +1,61 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+
+namespace Aproksymacja_funkcjami_sklejanymi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SplineApproximation sp = new SplineApproximation();
+            double h = 1;
+            double baseptk = 0;
+            double x = 1.1;
+            int n = 6;
+            Dictionary<double, double> dic = new Dictionary<double, double>();
+            dic.Add(0, Math.Sin(0));
+            dic.Add(0.523, Math.Sin(0.523));
+            dic.Add(1.57, Math.Sin(1.57));
+            dic.Add(0.785, Math.Sin(0.785));
+            dic.Add(1.046, Math.Sin(1.046));
+            dic.Add(1.3089, Math.Sin(1.3089));
+            dic.Add(0.2617, Math.Sin(0.2617));
+            dic.Add(0.139, Math.Sin(0.139));
+            //Console.WriteLine(sp.BaseSpline(baseptk,h,x));
+            double[] xi = sp.Approximate(0,2*Math.PI,n,dic);
+            //   for (int i = 0; i < n+3; i++)
+            //  {
+            //    for (int j = 0; j < n+3; j++)
+            //     {
+            //         Console.Write(string.Format("{0} ", xi[i, j]));
+            //     }
+            //     Console.Write(Environment.NewLine + Environment.NewLine);
+            // }
+            //  Console.ReadLine();
+            Console.WriteLine("Współczynniki");
+            for (int i = 0; i < xi.Length; i++)
+                Console.WriteLine(xi[i]);
+
+
+
+            double[] sinx = new double[6];
+            double wartosc1 = Math.Sin(Math.PI / 2);
+            double wartosc2 = xi[0]*sp.BaseSpline(0 - h, h, Math.PI / 2) + xi[1] * sp.BaseSpline(0, h, Math.PI / 2) + xi[2] * sp.BaseSpline(0 + h, h, Math.PI / 2) + xi[3] * sp.BaseSpline(0 + 2*h, h, Math.PI / 2) + xi[4] * sp.BaseSpline(0 + 3*h, h, Math.PI / 2);
+            double wartosc3 = Math.Sin(0);
+            double wartosc4 = xi[0] * sp.BaseSpline(0 - h, h, 0) + xi[1] * sp.BaseSpline(0, h, 0) + xi[2] * sp.BaseSpline(0 + h, h, 0) + xi[3] * sp.BaseSpline(0 + 2 * h, h, 0) + xi[4] * sp.BaseSpline(0 + 3 * h, h, 0);
+            Console.WriteLine("Porównanie wartości");
+            Console.WriteLine(wartosc1);
+            Console.WriteLine(wartosc2);
+            Console.WriteLine(wartosc3);
+            Console.WriteLine(wartosc4);
+
+
+
+
+
+        }
+    }
+}
---
+++ Aproksymacja funkcjami sklejanymi/Aproksymacja funkcjami sklejanymi/SplineApproximation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double.Solvers;
+
+namespace Aproksymacja_funkcjami_sklejanymi
+{
+    public class SplineApproximation
+    {
+        public double[] Approximate(double a, double b, int n, Dictionary<double,double> points)
+        {
+            //przygotowanie tablic i przedziałów
+            double h = (b - a) / n;
+            double[] x = new double[n + 3];
+            double sum = 0;
+            x[0] = a - h; x[n + 2] = b + h;
+            for (int i = 1; i < n + 2; i++)
+                x[i] = x[i - 1] + h;
+            double[,] factors = new double[n + 3, n + 3];
+            //współczynniki
+            for(int i = 0; i < n + 3; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < n + 3; j++)
+                {
+                    sum = 0;
+                    foreach (var ptk in points)
+                        sum += BaseSpline(x[i], h, ptk.Key) * BaseSpline(x[j], h, ptk.Key);
+                    factors[i, j] = sum;
+                }                   
+            }
+            //wyrazy wolne
+            //sum = 0;
+            double[] constantTerms = new double[n + 3];
+            for(int k = 0; k<n+3; k++)
+            {
+                sum = 0;
+                foreach (var ptk in points)
+                    sum += ptk.Value * BaseSpline(x[k], h, ptk.Key);
+                constantTerms[k] = sum;
+            }
+            //obliczanie ukladu rownan;
+            var A = Matrix<double>.Build.DenseOfArray(factors);
+            var B = Vector<double>.Build.Dense(constantTerms);
+            var wynik = A.SolveIterative(B, new MlkBiCgStab()); ;
+
+            return wynik.AsArray();
+        }
+        
+        public double BaseSpline(double baseptk, double h, double x)
+        {
+            double constant = (1 / Math.Pow(h, 3));
+            if ((x >= baseptk - 2 * h) && (x <= baseptk - h))
+                return constant * Math.Pow(x - (baseptk - 2 * h),3);
+            if((x >= baseptk - h) && (x <= baseptk))
+                return constant * (Math.Pow(h, 3) + (3*Math.Pow(h, 2)*(x - (baseptk - h))) + (3*h*Math.Pow(x - (baseptk - h), 2)) - (3*Math.Pow(x - (baseptk - h), 3)));
+            if ((x <= baseptk + h) && (x >= baseptk))
+                return constant * (Math.Pow(h, 3) + 3*Math.Pow(h, 2)*((baseptk + h) - x) + 3*h*Math.Pow((baseptk + h) - x, 2) - 3*Math.Pow((baseptk + h) - x, 3));
+            if ((x <= baseptk + 2 * h) && (x >= baseptk + h))
+                return constant * Math.Pow((baseptk + 2 * h) - x,3);
+            else
+                return 0;  
+        }
+    }
+
+
+}
