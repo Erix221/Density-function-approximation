7b1eca6f4d444166c282e9704d2c40e76d67ddec
Eryk Olszewski <erix2211@wp.pl>
2020-12-06 16:48:12 +0100

    Dokładniejszy test pokazujący różnice w czasie działania poprawionej funkcji.

--- Aproksymacja funkcjami sklejanymi/Aproksymacja funkcjami sklejanymi/Program.cs
+++ Aproksymacja funkcjami sklejanymi/Aproksymacja funkcjami sklejanymi/Program.cs
@@ -11,13 +11,18 @@ namespace Aproksymacja_funkcjami_sklejanymi
         static void Main(string[] args)
         {           
             SplineApproximation sp = new SplineApproximation();
-            double[,] Testresult = sp.TestSplineSinus();
+            double[] points = sp.GetPointsforTest();
+            double[,] Testresult = sp.TestSplineSinus(points);
+            double[,] Testresult1 = sp.TestSplineSinus1(points);
 
             Console.WriteLine("Porównanie wartości");
             for (int i = 0; i < 100; i++)
             {
-                Console.WriteLine("Wartość aproksymowana: " + Testresult[i, 0] + "| Wartość dokładna: " + Testresult[i, 1]);
-                Console.WriteLine("Różnica: " + (Testresult[i, 1] - Testresult[i, 0]));
+                Console.WriteLine("Wartość aproksymowana(stara metoda): " + Testresult[i, 0] + "| Wartość dokładna: " + Testresult[i, 1]);
+                Console.WriteLine("Wartość aproksymowana(nowa metoda): " + Testresult1[i, 0] + "| Wartość dokładna: " + Testresult1[i, 1]);
+                Console.WriteLine("Różnica(stara): " + (Testresult[i, 1] - Testresult[i, 0]));
+                Console.WriteLine("Różnica(nowa): " + (Testresult1[i, 1] - Testresult1[i, 0]));
+                Console.WriteLine("Różnica między metodami(nowa - stara): " + (Testresult1[i, 0] - Testresult[i, 0]));
             }
 
         }
--- Aproksymacja funkcjami sklejanymi/Aproksymacja funkcjami sklejanymi/SplineApproximation.cs
+++ Aproksymacja funkcjami sklejanymi/Aproksymacja funkcjami sklejanymi/SplineApproximation.cs
@@ -4,6 +4,7 @@ using System.Text;
 using MathNet.Numerics;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double.Solvers;
+using System.Diagnostics;
 
 namespace Aproksymacja_funkcjami_sklejanymi
 {
@@ -51,6 +52,24 @@ namespace Aproksymacja_funkcjami_sklejanymi
             return factors;
         }
 
+        private double[,] GetFactors1(double[] array, double[] BasePtks, double h)
+        {
+            double sum = 0;
+            double[,] factors = new double[NumberOfIntervals + 3, NumberOfIntervals + 3];
+            for (int i = 0; i < NumberOfIntervals + 3; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < NumberOfIntervals + 3; j++)
+                {
+                    sum = 0;
+                    foreach (var ptk in X)
+                        sum += BaseSpline1(BasePtks[i], h, ptk) * BaseSpline1(BasePtks[j], h, ptk);
+                    factors[i, j] = sum;
+                }
+            }
+            return factors;
+        }
+
         private double[] GetConstantTerms(double[] BasePtks, double h)
         {
             double sum = 0;
@@ -65,6 +84,36 @@ namespace Aproksymacja_funkcjami_sklejanymi
             return constantTerms;
         }
 
+        private double[] GetConstantTerms1(double[] BasePtks, double h)
+        {
+            double sum = 0;
+            double[] constantTerms = new double[NumberOfIntervals + 3];
+            for (int i = 0; i < NumberOfIntervals + 3; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < X.Length; j++)
+                    sum += Y[j] * BaseSpline1(BasePtks[i], h, X[j]);
+                constantTerms[i] = sum;
+            }
+            return constantTerms;
+        }
+
+        public double[] Approximate1()
+        {
+            double h = (EndOfInterval - StartOfInterval) / NumberOfIntervals;
+            double[] BasePtks = new double[NumberOfIntervals + 3];
+            BasePtks[0] = StartOfInterval - h; BasePtks[NumberOfIntervals + 2] = EndOfInterval + h;
+            for (int i = 1; i < NumberOfIntervals + 2; i++)
+                BasePtks[i] = BasePtks[i - 1] + h;
+
+            double[,] factors = GetFactors1(X, BasePtks, h);
+            double[] constantTerms = GetConstantTerms1(BasePtks,h);
+
+            var A = Matrix<double>.Build.DenseOfArray(factors);
+            var B = Vector<double>.Build.Dense(constantTerms);
+            return A.SolveIterative(B, new MlkBiCgStab()).ToArray(); 
+        }
+
         public double[] Approximate()
         {
             double h = (EndOfInterval - StartOfInterval) / NumberOfIntervals;
@@ -74,27 +123,51 @@ namespace Aproksymacja_funkcjami_sklejanymi
                 BasePtks[i] = BasePtks[i - 1] + h;
 
             double[,] factors = GetFactors(X, BasePtks, h);
-            double[] constantTerms = GetConstantTerms(BasePtks,h);
+            double[] constantTerms = GetConstantTerms(BasePtks, h);
 
             var A = Matrix<double>.Build.DenseOfArray(factors);
             var B = Vector<double>.Build.Dense(constantTerms);
-            return A.SolveIterative(B, new MlkBiCgStab()).ToArray(); 
+            return A.SolveIterative(B, new MlkBiCgStab()).ToArray();
+        }
+
+        private static double BaseSpline1(double baseptk, double h, double x)
+        {
+            double h2 = h * h;
+            double h3 = h2 * h;
+            double constant = (1 / h3);
+            double interval1 = x - (baseptk - 2*h);
+            double interval2 = x - (baseptk - h);
+            double interval3 = (baseptk + h) - x;
+            double interval4 = (baseptk + 2*h) - x;
+            if ((x >= baseptk - 2 * h) && (x <= baseptk - h))
+                return constant * (interval1* interval1* interval1);
+            if ((x >= baseptk - h) && (x <= baseptk))
+                return constant * (h3 + 3*((h2 * (interval2)) + (h * (interval2* interval2)) - (interval2* interval2* interval2)));
+            if ((x <= baseptk + h) && (x >= baseptk))
+                return constant * (h3 + 3 * (h2 * (interval3) + h *(interval3* interval3) - (interval3* interval3* interval3)));
+            if ((x <= baseptk + 2 * h) && (x >= baseptk + h))
+                return constant * (interval4* interval4* interval4);
+            else
+                return 0;
+
         }
 
         private static double BaseSpline(double baseptk, double h, double x)
         {
             double constant = (1 / Math.Pow(h, 3));
             if ((x >= baseptk - 2 * h) && (x <= baseptk - h))
-                return constant * Math.Pow(x - (baseptk - 2 * h),3);
-            if((x >= baseptk - h) && (x <= baseptk))
-                return constant * (Math.Pow(h, 3) + (3*Math.Pow(h, 2)*(x - (baseptk - h))) + (3*h*Math.Pow(x - (baseptk - h), 2)) - (3*Math.Pow(x - (baseptk - h), 3)));
+                return constant * Math.Pow(x - (baseptk - 2 * h), 3);
+            if ((x >= baseptk - h) && (x <= baseptk))
+                return constant * (Math.Pow(h, 3) + (3 * Math.Pow(h, 2) * (x - (baseptk - h))) + (3 * h * Math.Pow(x - (baseptk - h), 2)) - (3 * Math.Pow(x - (baseptk - h), 3)));
             if ((x <= baseptk + h) && (x >= baseptk))
-                return constant * (Math.Pow(h, 3) + 3*Math.Pow(h, 2)*((baseptk + h) - x) + 3*h*Math.Pow((baseptk + h) - x, 2) - 3*Math.Pow((baseptk + h) - x, 3));
+                return constant * (Math.Pow(h, 3) + 3 * Math.Pow(h, 2) * ((baseptk + h) - x) + 3 * h * Math.Pow((baseptk + h) - x, 2) - 3 * Math.Pow((baseptk + h) - x, 3));
             if ((x <= baseptk + 2 * h) && (x >= baseptk + h))
-                return constant * Math.Pow((baseptk + 2 * h) - x,3);
+                return constant * Math.Pow((baseptk + 2 * h) - x, 3);
             else
-                return 0;  
+                return 0;
         }
+
+
         private double FunctionValue(double[] result, double x)
         {
             double value = 0;
@@ -102,33 +175,67 @@ namespace Aproksymacja_funkcjami_sklejanymi
                 value += result[i] * BaseSpline(StartOfInterval + (H * (-1 + i)), H, x);
             return value;
         }
-        public double[,] TestSplineSinus()
+
+        private double FunctionValue1(double[] result, double x)
+        {
+            double value = 0;
+            for (int i = 0; i < result.Length; i++)
+                value += result[i] * BaseSpline1(StartOfInterval + (H * (-1 + i)), H, x);
+            return value;
+        }
+
+        public double[] GetPointsforTest() // losuje x w konkretnym przedziale do testu
         {
             Random r = new Random();
-            double range = Math.PI / 2;
+            double range = 2*Math.PI;
             double rDouble = r.NextDouble() * range; //for doubles
-            double[] x = new double[100];
-            for(int i = 0; i < 100; i++)
+            double[] x = new double[1000000];
+            for (int i = 0; i < 1000000; i++)
             {
-                x[i] = r.NextDouble() * range;
+                x[i] =(r.NextDouble() * range);
             }
-            double[] y = new double[100];
-            for (int i = 0; i < 100; i++)
+            return x;
+        }
+        public double[,] TestSplineSinus(double[] x)
+        {
+            double[] y = new double[1000000];
+            for (int i = 0; i < 1000000; i++) // wyliczam wartości Sinusa
             {
                 y[i] = Math.Sin(x[i]);
             }
-            SplineApproximation sp = new SplineApproximation(0, Math.PI / 2, 6, x, y);
+            SplineApproximation sp = new SplineApproximation(0, 2 * Math.PI, 6, x, y); // zmieniac trzecią wartość, jest to numer przedzialów
             double h = (sp.EndOfInterval - sp.StartOfInterval) / sp.NumberOfIntervals;
+            var sw1 = Stopwatch.StartNew();
+            double res1 = 0;
             double[] result = sp.Approximate();
-            for (int i = 0; i < result.Length; i++)
-                Console.WriteLine("Współczynnik: "+ i +" " +result[i] + " Ilosc wspolczynników: " + result.Length);
-            double[,] sinsprox = new double[100, 2];
-            for(int i = 0; i<100; i++)
+            Console.WriteLine("Old: " + sw1.ElapsedMilliseconds + " ms:  " + res1);
+            double[,] sinsprox = new double[1000000, 2];
+            for (int i = 0; i < 1000000; i++)
             {
-                Console.WriteLine("Punkt:" + x[i]);
-                //sinsprox[i, 0] = result[0] * BaseSpline(sp.StartOfInterval - h, h, x[i]) + result[1] * BaseSpline(sp.StartOfInterval, h, x[i]) + result[2] * BaseSpline(sp.StartOfInterval + h, h, x[i]) + result[3] * BaseSpline(sp.StartOfInterval + 2 * h, h, x[i]) + result[4] * BaseSpline(sp.StartOfInterval + 3 * h, h, x[i]) + result[5] * BaseSpline(sp.StartOfInterval + 4 * h, h, x[i]) + result[6] * BaseSpline(sp.StartOfInterval + 5 * h, h, x[i]) + result[7] * BaseSpline(sp.StartOfInterval + 6 * h, h, x[i]);
-                sinsprox[i, 0] = sp.FunctionValue(result,x[i]);
-                sinsprox[i, 1] = Math.Sin(x[i]);
+                sinsprox[i, 0] = sp.FunctionValue(result, x[i]); // szybsze rozwiazanie
+                sinsprox[i, 1] = y[i];
+            }
+            return sinsprox;
+        }
+
+        public double[,] TestSplineSinus1(double[] x)
+        {
+            double[] y = new double[1000000];
+            for (int i = 0; i < 1000000; i++) // wyliczam wartości Sinusa
+            {
+                y[i] = Math.Sin(x[i]);
+            }
+            SplineApproximation sp = new SplineApproximation(0, 2 * Math.PI, 6, x, y); // zmieniac trzecią wartość, jest to numer przedzialów
+            double h = (sp.EndOfInterval - sp.StartOfInterval) / sp.NumberOfIntervals;
+            var sw1 = Stopwatch.StartNew();
+            double res1 = 0;
+            double[] result1 = sp.Approximate1();
+            Console.WriteLine("New: " + sw1.ElapsedMilliseconds + " ms:  " + res1);
+            double[,] sinsprox = new double[1000000, 2];
+            for (int i = 0; i < 1000000; i++)
+            {
+                sinsprox[i, 0] = sp.FunctionValue1(result1, x[i]); // szybsze rozwiazanie
+                sinsprox[i, 1] = y[i];
             }
             return sinsprox;
         }
