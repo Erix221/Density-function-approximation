8936d8df7c41c5ac52d23cdded2ffc295d88bdd5
Eryk Olszewski <erix2211@wp.pl>
2021-06-17 17:19:58 +0200

    Dodanie klas.

---
+++ Aproksymacja funkcjami sklejanymi/Aproksymacja funkcjami sklejanymi/CubicFunction.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace Spline_Approximation_dll
+{
+    public class CubicFunction
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public double D { get; set; }
+
+        public CubicFunction()
+        {
+
+        }
+
+        public CubicFunction(double a, double b, double c, double d)
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+        }
+
+        public double Value(double x)
+        {
+            return A * x * x * x + B * x * x + C * x + D;
+        }
+
+        public double FirstDerivative(double x)
+        {
+            return 3 * A * x * x + 2 * B * x + C;
+        }
+
+        public static CubicFunction operator +(CubicFunction cubic, double add)
+        {
+            return new CubicFunction(cubic.A + add, cubic.B + add,
+                                     cubic.C + add, cubic.D + add);
+        }
+
+        public static CubicFunction operator +(CubicFunction cubic, CubicFunction cubic2)
+        {
+            return new CubicFunction(cubic.A + cubic2.A, cubic.B + cubic2.B,
+                                     cubic.C + cubic2.C, cubic.D + cubic2.D);
+        }
+
+        public static CubicFunction operator -(CubicFunction cubic, double subtract)
+        {
+            return new CubicFunction(cubic.A - subtract, cubic.B - subtract,
+                                     cubic.C - subtract, cubic.D - subtract);
+        }
+        public static CubicFunction operator -(CubicFunction cubic, CubicFunction cubic2)
+        {
+            return new CubicFunction(cubic.A - cubic2.A, cubic.B - cubic2.B,
+                                     cubic.C - cubic2.C, cubic.D - cubic2.D);
+        }
+
+        public static CubicFunction operator *(CubicFunction cubic, double multiply)
+        {
+            return new CubicFunction(cubic.A * multiply, cubic.B * multiply,
+                                      cubic.C * multiply, cubic.D * multiply);
+        }
+
+        public static CubicFunction operator /(CubicFunction cubic, double divide)
+        {
+            if (divide == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new CubicFunction(cubic.A / divide, cubic.B / divide,
+                                     cubic.C / divide, cubic.D / divide);
+        }
+
+        public double[] CalculateRealRoots()
+        {
+            if (A != 0)
+            {
+                double f, g, h;
+                f = (C - B * B / 3 / A) / A;
+                g = ((2 * B * B / 9 / A - C) / A * B / 3 + D) / A;
+                h = (g * g / 4) + (f * f * f / 27);
+                if (h > 0)
+                {
+                    return OneRoot(g, h);
+
+                }
+                else if (f == 0 && g == 0)
+                {
+                    return TripleRoot();
+                }
+                else
+                {
+                    return ThreeRoots(g, h);
+                }
+            }
+            return new double[0];
+
+        }
+        private double[] OneRoot(double g, double h)
+        {
+            double x1 = -(g / 2) + Math.Sqrt(h);
+            double x2 = -(g / 2) - Math.Sqrt(h);
+            double x3 = B / (3 * A);
+            double root1 = Math.Cbrt(x1) + Math.Cbrt(x2) - x3;
+            double[] roots = new double[1];
+            roots[0] = root1;
+            return roots;
+        }
+
+        private double[] TripleRoot()
+        {
+            double[] roots = { Math.Cbrt(D / A) };
+            return roots;
+        }
+
+        private double[] ThreeRoots(double g, double h)
+        {
+            double i = Math.Sqrt((g * g / 4) - h);
+            double j = Math.Cbrt(i);
+            double k = Math.Acos(-g / (2 * i));
+            double m = Math.Cos(k / 3);
+            double n = Math.Sqrt(3) * Math.Sin(k / 3);
+            double p = (-B) / (3 * A);
+            double root1 = (2 * j * m) + p;
+            double root2 = (-j * (m + n)) + p;
+            double root3 = (-j * (m - n)) + p;
+            double[] roots = new double[3];
+            roots[0] = root1;
+            roots[1] = root2;
+            roots[2] = root3;
+            return roots;
+        }
+    }
+}
---
+++ Aproksymacja funkcjami sklejanymi/Aproksymacja funkcjami sklejanymi/ThirdDegreeSpline.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spline_Approximation_dll
+{
+    public class ThirdDegreeSpline
+    {
+        public double StartOfInterval { get; set; }
+        public double EndOfInterval { get; set; }
+        public int NumberOfIntervals { get; set; }
+        public double H { get; set; }
+        public double[] Knots { get; set; }
+        public CubicFunction[] CubicFunctions { get; set; }
+        public CubicFunction ZeroFunction { get; set; }
+
+        public ThirdDegreeSpline(double start, double end, int n, CubicFunction[] functions)
+        {
+            StartOfInterval = start;
+            EndOfInterval = end;
+            NumberOfIntervals = n;
+            H = Math.Round((EndOfInterval - StartOfInterval) / NumberOfIntervals,15);
+            CubicFunctions = functions;
+            ZeroFunction = new CubicFunction(0, 0, 0, 0);
+            Knots = CalculateKnots();
+        }
+
+
+        public double[] CalculateRanges(ThirdDegreeSpline spline)
+        {
+            double[] FirstBasePtks = Knots;
+            double[] SecondBasePtks = spline.Knots;
+            List<double> result = FirstBasePtks.Concat(SecondBasePtks).ToList();
+            result.Sort();
+
+            double FirstPoint = FirstBasePtks[0] >= SecondBasePtks[0] ? FirstBasePtks[0] : SecondBasePtks[0];
+            double LastPoint = FirstBasePtks[^1] <= SecondBasePtks[^1] ? FirstBasePtks[^1] : SecondBasePtks[^1];
+            int startIndex = result.BinarySearch(FirstPoint);
+            int endIndex = result.BinarySearch(LastPoint);
+            List<double> final = result.GetRange(startIndex, endIndex - startIndex);
+            return final.Distinct().ToArray();
+        }
+
+        public double[] CalculateIntersectionPoints(ThirdDegreeSpline spline)
+        {
+            if (CheckforOverlap(spline))
+            {
+                double[] Ranges = CalculateRanges(spline);
+                CubicFunction[] functions = new CubicFunction[Ranges.Length - 1];
+                for (int i = 0; i < Ranges.Length - 1; i++)
+                {
+                    functions[i] = GetFunction((Ranges[i + 1] + Ranges[i])/2) 
+                                   - spline.GetFunction((Ranges[i + 1] + Ranges[i]) / 2);
+                }                    
+                    
+                ThirdDegreeSpline newSpline = new ThirdDegreeSpline(Ranges[0], Ranges[^1],
+                                                                    Ranges.Length, functions);
+                return newSpline.CalculateIntersectionPointsOX();
+            }
+            double[] roots = new double[0];
+            return roots;
+        }
+
+        public double[] CalculateIntersectionPointsOX()
+        {
+            List<double> finalroots = new List<double>();
+            foreach (var func in CubicFunctions)
+            {
+                double[] roots = func.CalculateRealRoots();
+                for (int i = 0; i < roots.Length; i++)
+                {
+                    if((roots[i] >= StartOfInterval) && (roots[i] <= EndOfInterval))
+                    finalroots.Add(roots[i]);
+                }
+                    
+            }                  
+                return finalroots.ToArray();
+        }
+
+        public bool CheckforOverlap(ThirdDegreeSpline spline)
+        {
+            return StartOfInterval < spline.EndOfInterval && spline.StartOfInterval < EndOfInterval;
+        }
+
+        private double[] CalculateKnots()
+        {
+            double[] knots = new double[NumberOfIntervals + 1];
+            for (int i = 0; i < NumberOfIntervals + 1; i++)
+                knots[i] = Math.Round(StartOfInterval + H * (i), 15);
+            return knots;
+        }
+
+        public double Value(double x)
+        {
+            int Check = CheckRange(x);
+            if ((Check >= 0) && (Check < CubicFunctions.Length))
+                return CubicFunctions[Check].Value(x);
+            else
+                return 0;
+        }
+
+        public double FirstDerivative(double x)
+        {
+            int Check = CheckRange(x);
+            if ((Check >= 0) && (Check < CubicFunctions.Length))
+                return CubicFunctions[Check].FirstDerivative(x);
+            else
+                return 0;
+        }
+
+        public int CheckRange(double x)
+        {
+            double result = ((x - StartOfInterval) / H);
+            if (result < 0)
+                return -1;
+            return (int)result;
+        }
+
+        public CubicFunction GetFunction(double x)
+        {
+            int Check = CheckRange(x);
+            if ((Check >= 0) && (Check < CubicFunctions.Length))
+                return CubicFunctions[Check];
+            else
+                return ZeroFunction;
+
+        }
+    }
+}
